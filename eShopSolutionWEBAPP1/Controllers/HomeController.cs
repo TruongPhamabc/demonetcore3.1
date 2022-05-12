c822dfb83954953660bd090d412801eba9b859a7
truongpv <Admin@DESKTOP-02ANM2G>
2022-05-12 04:42:58 -0700

    commit

--- eShopSolutionWEBAPP1/Controllers/HomeController.cs
+++ eShopSolutionWEBAPP1/Controllers/HomeController.cs
@@ -31,6 +31,7 @@ namespace eShopSolutionWEBAPP1.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            // commit dau tien
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
