ab2463a58d04e1e72da5eda9b2c125ca969138af
truongpv <Admin@DESKTOP-02ANM2G>
2022-05-12 04:42:12 -0700

    test

--- eShopSolutionWEBAPP1/Controllers/HomeController.cs
+++ eShopSolutionWEBAPP1/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace eShopSolutionWEBAPP1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
